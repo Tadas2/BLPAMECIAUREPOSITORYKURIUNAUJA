42d3a7e84007540cadaf971c71dba47d3fae6c49
Tadas2 <tadas.zilinskas74@gmail.com>
2019-03-28 11:10:06 +0200

    ketvirtos dienos paxmielas

---
+++ app/classes/item/User.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App;
+
+Class User {
+
+    private $data;
+
+    const GAY = 'g';
+    const STRAIGHT = 's';
+    const BISEXUAL = 'b';
+    const GENDER_MALE = 'm';
+    const GENDER_FEMALE = 'f';
+
+    public function __construct($data = null) {
+        if (!$data) {
+            $this->data = [
+                'username' => null,
+                'email' => null,
+                'full_name' => null,
+                'age' => null,
+                'gender' => null,
+                'orientation' => null,
+                'photo' => null,
+            ];
+        } else {
+            $this->setData($data);
+        }
+    }
+
+    public function setUsername(string $username) {
+        $this->data['username'] = $username;
+    }
+
+    public function setEmail(string $email) {
+        $this->data['email'] = $email;
+    }
+
+    public function setAge(int $age) {
+        $this->data['age'] = $age;
+    }
+
+    public function setGender(string $gender) {
+        if (in_array($gender, [$this::GENDER_MALE, $this::GENDER_FEMALE])) {
+            $this->data['gender'] = $gender;
+        }
+    }
+
+    /**
+     * Gets the name of given object.
+     * @return type string
+     */
+    public function setOrientation($orientation) {
+        if (in_array($orientation, [$this::GAY, $this::STRAIGHT, $this::BISEXUAL])) {
+            $this->data['orientation'] = $orientation;
+        }
+    }
+
+    public function setPhoto(string $photo) {
+        $this->data['photo'] = $photo;
+    }
+
+    public function getUsername() {
+        return $this->data['username'];
+    }
+
+    public function getEmail() {
+        return $this->data['email'];
+    }
+
+    public function getFullName() {
+        return $this->data['full_name'];
+    }
+
+    public function getAge() {
+        return $this->data['age'];
+    }
+
+    public function getGender() {
+        return $this->data['gender'];
+    }
+
+    public function getOrientation() {
+        return $this->data['orientation'];
+    }
+
+    public function getPhoto() {
+        return $this->data['photo'];
+    }
+
+    public function setData(array $data) {
+        $this->setUsername($data['username'] ?? '');
+        $this->setEmail($data['email'] ?? '');
+        $this->setFullName($data['full_name'] ?? '');
+        $this->setAge($data['age'] ?? null);
+        $this->setGender($data['gender'] ?? '');
+        $this->setOrientation($data['orientation'] ?? '');
+        $this->setPhoto($data['photo'] ?? '');
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+}
--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,59 @@
 <?php
-
 require '../bootloader.php';
+
+$form = [
+    'fields' => [
+        'pavadinimas' => [
+            'name' => 'pavadinimas',
+            'label' => 'Iveskite pavadinima ',
+            'type' => 'text',
+            'placeholder' => 'Gerimo pavadinimas',
+            'validate' => [
+                'validate_not_empty',
+            ],
+        ],
+        'amount_ml' => [
+            'name' => 'amount_ml',
+            'label' => 'Iveskite kieki ',
+            'type' => 'number',
+            'placeholder' => 'Gerimo turis',
+            'validate' => [
+                'validate_not_empty',
+            ],
+        ],
+        'abarot' => [
+            'name' => 'abarot',
+            'label' => 'Iveskite abarota ',
+            'type' => 'text',
+            'placeholder' => 'Gerimo abarotai',
+            'validate' => [
+                'validate_not_empty',
+            ],
+        ],
+        'image' => [
+            'name' => 'image',
+            'label' => 'Iveskite paveiksliuko url ',
+            'type' => 'file',
+            'placeholder' => '',
+          'validate' => [
+              'validate_field_file',
+              // 'validate_not_empty',
+          ],
+        ]
+    ],
+    'buttons' => [
+        'submit' => [
+            'text' => 'Submit'
+        ]
+    ],
+    'callbacks' => [
+        'success' => [
+            'form_success'
+        ],
+        'fail' => []
+    ]
+];
+
 
 $db = new Core\FileDB(ROOT_DIR . '/app/files/db.txt');
 $db->save();
@@ -10,28 +63,69 @@ $kokteilis = new \App\Item\Gerimas([
     'amount_ml' => 600,
     'abarot' => 00.00,
     'image' => 'images/random.png'
-    ]);
+        ]);
 
 $vynas = new \App\Item\Gerimas([
     'name' => 'x-failai',
     'amount_ml' => 300,
     'abarot' => 13.00,
     'image' => 'images/xfailai.jpg'
-    ]);
+        ]);
 
 $dege = new \App\Item\Gerimas([
     'name' => 'MILK',
     'amount_ml' => 500,
     'abarot' => 00.00,
     'image' => 'images/milk.jpg'
-    ]);
+        ]);
 
 $pinakolada = new \App\Item\Gerimas([
     'name' => 'Peni kolada',
     'amount_ml' => 200,
     'abarot' => 15.00,
     'image' => 'images/pina.jpg'
+        ]);
+
+function form_success($safe_input, $form) {
+    $db = new Core\FileDB(ROOT_DIR . '/app/files/db.txt');
+    $model_gerimai = new App\model\ModelGerimai($db, 'kokteiliai');
+    $gerimas = new \App\Item\Gerimas([
+        'name' => $safe_input['pavadinimas'],
+        'amount_ml' => $safe_input['amount_ml'],
+        'abarot' => $safe_input['abarot'],
+        'image' => $safe_input['image'],
     ]);
+    $id = time() . '_' . rand(0, 10000);
+    $model_gerimai->insert($id, $gerimas);
+}
+
+function validate_form_img(&$safe_input, &$form) {
+    $file_saved_url = save_file($safe_input['img']);
+    if ($file_saved_url) {
+        $safe_input['img'] = $file_saved_url;
+        return true;
+    } else {
+        $form['error_msg'] = 'blogas failas';
+    }
+}
+function save_file($file, $dir = 'uploads', $allowed_types = ['image/png', 'image/jpeg', 'image/gif']) {
+    
+    if ($file['error'] == 0 && in_array($file['type'], $allowed_types)) {
+        $target_file_name = microtime() . '-' . strtolower($file['name']);
+        $target_path = $dir . '/' . $target_file_name;
+        if (move_uploaded_file($file['tmp_name'], $target_path)) {
+            return $target_file_name;
+        }
+    }
+    return false;
+}
+
+$model_gerimai = new App\model\ModelGerimai($db, 'Gerimai');
+
+if (!empty($_POST)) {
+    $safe_input = get_safe_input($form);
+    $success = validate_form($safe_input, $form);
+}
 
 $model_gerimai = new App\model\ModelGerimai($db, 'kokteiliai');
 $model_gerimai->insert('kokteilis', $kokteilis);
@@ -45,12 +139,15 @@ $model_gerimai->insert('pinakolada', $pinakolada);
     </head>
     <body>
         <?php foreach ($model_gerimai->loadAll() as $gerimas): ?>
-        <div>
-            <p>Pavadinimas: <?php print $gerimas->getName(); ?></p>
-            <p>Kiekis: <?php print $gerimas->getAmount();?></p>
-            <p>Abarotai: <?php print $gerimas->getAbarot();?></p>
-            <p><img src="<?php print $gerimas->getImage();?>"></p>
+            <div>
+                <p>Pavadinimas: <?php print $gerimas->getName(); ?></p>
+                <p>Kiekis: <?php print $gerimas->getAmount(); ?></p>
+                <p>Abarotai: <?php print $gerimas->getAbarot(); ?></p>
+                <p><img src="<?php print $gerimas->getImage(); ?>"></p>
             <?php endforeach; ?>
+        </div>
+        <div>
+            <?php require ROOT_DIR . '/core/views/form.php'; ?>
         </div>
     </body>
 </html>
