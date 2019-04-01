d9c3062679f905e666137ffdb01dfdf1f48af974
Tadas2 <tadas.zilinskas74@gmail.com>
2019-04-01 13:38:43 +0300

    gaidys

--- app/classes/item/party.php
+++ app/classes/item/party.php
@@ -20,8 +20,25 @@ class Party {
         $this->users = $this->model_user->loadAll();
         $this->gerimai = $this->model_gerimai->loadAll();
     }
+
     public function getUserCount() {
         return count($this->users);
+    }
+
+    public function getPureAlchoholTotal() {
+        $pure_alko = 0;
+        foreach ($this->gerimai as $gerimas) {
+            $pure_alko += $gerimas->getAmount() * $gerimas->getAbarot() / 100;
+            
+            return $pure_alko;
+        }
+    }
+    public function getPureAlchoholPerUser() {
+        if($this->getUserCount > 0) {
+        return $this->getPureAlchoholTotal() / $this->getUserCount();
+        } else {
+            return false;
+        }
     }
 
 }
